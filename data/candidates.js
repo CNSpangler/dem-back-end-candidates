b55a3131ff640ac71e0f3a3392c995ac4931a71c
Chelsea Spangler <chelseanspangler@gmail.com>
2020-02-21 12:08:51 -0800

    add data

--- data/candidates.js
+++ data/candidates.js
@@ -3,7 +3,7 @@ module.exports = [
         name: `Pete Buttigieg`,
         born: 1982,
         running: true,
-        identity: `Millenial`,
+        identity: `Millennial`,
         img: `https://i.redd.it/o2c3hcznqgp31.jpg`
     },
 
@@ -53,5 +53,133 @@ module.exports = [
         running: false,
         identity: `Radical Vegan`,
         img: `https://www.nj.com/resizer/OX5AH1E6Nx1uQDwBfI00B4WS444=/450x0/smart/arc-anglerfish-arc2-prod-advancelocal.s3.amazonaws.com/public/UYGTAVUSIBC3DHV7BVOSSYBIZU.png`
+    },
+
+    {
+        name: `Michael Bloomberg`,
+        born: 1942,
+        running: true,
+        identity: `Egomaniacal Billionaire`,
+        img: `https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F200131115010-101-michael-bloomberg-lead-image.jpg`
+    },
+
+    {
+        name: `Tom Steyer`,
+        born: 1957,
+        running: true,
+        identity: `Egomaniacal Billionaire`,
+        img: `https://www.mediaite.com/wp-content/uploads/2019/11/snl-dem-debate.jpg`
+    },
+
+    {
+        name: `Marianne Williamson`,
+        born: 1952,
+        running: false,
+        identity: `Crazypants`,
+        img: `https://pbs.twimg.com/ext_tw_video_thumb/1178289032989696000/pu/img/5lL4nqVIMoINR55s.jpg`
+    },
+
+    {
+        name: `Tulsi Gabbard`,
+        born: 1981,
+        running: true,
+        identity: `War Criminal`,
+        img: `https://i.ytimg.com/vi/SzZegojwwqY/hqdefault.jpg`
+    },
+
+    {
+        name: `Kamala Harris`,
+        born: 1964,
+        running: false,
+        identity: `America's Funt`,
+        img: `https://img.huffingtonpost.com/asset/5d90dc242200005a00df917f.jpeg?cache=cve6kvq5xu&ops=1778_1000`
+    },
+
+    {
+        name: `John Delaney`,
+        born: 1963,
+        running: false,
+        identity: `Anonymous White Man`,
+        img: `https://s.abcnews.com/images/ThisWeek/190113_tw_delaney3_hpMain_16x9_992.jpg`
+    },
+
+    {
+        name: `Michael Bennet`,
+        born: 1964,
+        running: false,
+        identity: `Anonymous White Man`,
+        img: `https://assets.forwardcdn.com/images/cropped/michael-bennet-official-photo-1561687536.jpg`
+    },
+
+    {
+        name: `Deval Patrick`,
+        born: 1956,
+        running: false,
+        identity: `Who??`,
+        img: `https://media.necn.com/2019/09/Deval-Patrick-12062018.JPG?resize=850%2C478`
+    },
+
+    {
+        name: `Julian Castro`,
+        born: 1974,
+        running: false,
+        identity: `The One that Got Away`,
+        img: `https://cdn.vox-cdn.com/thumbor/LVMupK276wJYurEXtiATXVnMaOc=/0x0:2294x1930/1200x800/filters:focal(1089x627:1455x993)/cdn.vox-cdn.com/uploads/chorus_image/image/65455816/Screen_Shot_2019_10_12_at_11.12.21_PM.0.png`
+    },
+
+    {
+        name: `Beto O'Rourke`,
+        born: 1972,
+        running: false,
+        identity: `In it...until he's not`,
+        img: `https://www.thewrap.com/wp-content/uploads/2019/03/Jimmy-Fallons-Beto-O-Rourke.jpg`
+    },
+
+    {
+        name: `Bill de Blasio`,
+        born: 1961,
+        running: false,
+        identity: `Anonymous White Man`,
+        img: `https://i.insider.com/5c69845f2628983b84537d94?width=1100&format=jpeg&auto=webp`
+    },
+
+    {
+        name: `Tim Ryan`,
+        born: 1973,
+        running: false,
+        identity: `Anonymous White Man`,
+        img: `https://cdnph.upi.com/svc/sv/upi/9251555458314/2019/1/0d7a5d83032354c4b569a45a1d491432/Rep-Tim-Ryan-shies-from-socialism-in-2020-run-for-president.jpg`
+    },
+
+    {
+        name: `John Hickenlooper`,
+        born: 1952,
+        running: false,
+        identity: `Anonymous White Man`,
+        img: `https://s.abcnews.com/images/Politics/john-hickenlooper-debate-01-gty-jef-190627_hpMain_16x9_992.jpg`
+    },
+
+    {
+        name: `Jay Inslee`,
+        born: 1951,
+        running: false,
+        identity: `Anonymous White Man`,
+        img: `https://storage.googleapis.com/afs-prod/media/25ad98053b06439e8140a815d51e4af6/800.jpeg`
+    },
+
+    {
+        name: `Eric Swalwell`,
+        born: 1980,
+        running: false,
+        identity: `Anonymous White Man`,
+        img: `https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F190708121711-01-eric-swalwell-2019.jpg`
+    },
+
+    {
+        name: `Bender`,
+        born: 2996,
+        running: false,
+        identity: `Definitely a Real Human`,
+        img: `https://i.pinimg.com/474x/79/b1/79/79b1794d2825c8761251d404365f4a8c.jpg`
     },
 ];
